2b450087a6b036060a2c325bd2783c32c272fd0c
ahmadabushams <175308714+ahmad345-hub@users.noreply.github.com>
2026-09-09 13:18:17 +0300

    add system prompt

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ import os
 import argparse
 from dotenv import load_dotenv
 from openai import OpenAI
+from prompts import system_prompt
 
 load_dotenv()
 
@@ -28,12 +29,14 @@ def main():
 
 
     messages = [
+        {"role": "system", "content": system_prompt},
         {"role": "user", "content": args.user_prompt},
     ]
 
     response = client.chat.completions.create(
-        model="openrouter/free",
-        messages=messages,
+    model="openrouter/free",
+    messages=messages,
+    temperature=0,
     )
 
     if response.usage is None:
