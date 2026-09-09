68b215d8f1d3a4dcddf62ef938b6489fd617f9f9
ahmadabushams <175308714+ahmad345-hub@users.noreply.github.com>
2026-09-09 13:28:13 +0300

    add function declaration

--- main.py
+++ main.py
@@ -1,8 +1,10 @@
 import os
 import argparse
+import json
 from dotenv import load_dotenv
 from openai import OpenAI
 from prompts import system_prompt
+from call_function import available_functions
 
 load_dotenv()
 
@@ -36,6 +38,7 @@ def main():
     response = client.chat.completions.create(
     model="openrouter/free",
     messages=messages,
+    tools=available_functions,
     temperature=0,
     )
 
@@ -47,7 +50,14 @@ def main():
         print(f"Prompt tokens: {response.usage.prompt_tokens}")
         print(f"Response tokens: {response.usage.completion_tokens}")
 
-    print(response.choices[0].message.content)
+    message = response.choices[0].message
+
+    if message.tool_calls:
+        for tool_call in message.tool_calls:
+            function_args = json.loads(tool_call.function.arguments or "{}")
+            print(f"Calling function: {tool_call.function.name}({function_args})")
+    else:
+       print(message.content)
 
 
 if __name__ == "__main__":
